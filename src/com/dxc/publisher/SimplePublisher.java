6d478c072196857cfa00d69779a88138884f49a3
tchatakondu <tchatakondu@IN-5CG0251NCM>
2020-09-19 11:08:01 +0530

    update-1

---
+++ src/com/dxc/publisher/SimplePublisher.java
@@ -0,0 +1,45 @@
+package com.dxc.publisher;
+
+import java.util.Properties;
+import java.util.Scanner;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+public class SimplePublisher {
+ public static void main(String[] args) {
+ String topicName = "";
+ if (args.length == 0) {
+ Scanner sc = new Scanner(System.in);
+ System.out.println("Enter topic name");
+ topicName = sc.next();
+ }
+ Properties props = new Properties();
+ props.put("bootstrap.servers", "localhost:9092");
+ props.put("acks", "all");
+ props.put("retries", 0);
+ props.put("batch.size", 16384);
+ props.put("linger.ms", 1);
+ props.put("buffer.memory", 33554432);
+ props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+ props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+ Producer<String, String> producer = new KafkaProducer<String,
+String>(props);
+ for (int i = 0; i < 10; i++)
+ producer.send(new ProducerRecord<String, String>(topicName,
+		 													Integer.toString(i),
+		 													Integer.toString(i)));
+
+ producer.send(new ProducerRecord<String, String>(topicName,"Welcome"), new Callback() {
+
+ @Override
+ public void onCompletion(RecordMetadata metadata, Exception exception) {
+ System.out.println("Finished");
+ 
+ }
+ });
+ System.out.println("Message sent successfully");
+ producer.close();
+ }
+} 
---
+++ src/com/dxc/subscriber/SimpleSubscriber.java
@@ -0,0 +1,45 @@
+package com.dxc.subscriber;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Scanner;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+public class SimpleSubscriber {
+ public static void main(String[] args) {
+ String topicName = "";
+ if(args.length == 0){
+ System.out.println("Enter topic name");
+ Scanner sc = new Scanner(System.in);
+ System.out.println("Enter topic name");
+ topicName = sc.next();
+ }
+ Properties props = new Properties();
+
+ props.put("bootstrap.servers", "localhost:9092");
+ props.put("group.id", "test");
+ props.put("enable.auto.commit", "true");
+ props.put("auto.commit.interval.ms", "1000");
+ props.put("session.timeout.ms", "30000");
+ props.put("key.deserializer",
+ "org.apache.kafka.common.serialization.StringDeserializer");
+ props.put("value.deserializer",
+ "org.apache.kafka.common.serialization.StringDeserializer");
+ KafkaConsumer<String, String> consumer = new KafkaConsumer
+ <String, String>(props);
+
+ consumer.subscribe(Arrays.asList(topicName));
+ 
+ System.out.println("Subscribed to topic " + topicName);
+ int i = 0;
+
+ while (true) {
+ ConsumerRecords<String, String> records = consumer.poll(100);
+ for (ConsumerRecord<String, String> record : records)
+
+ System.out.printf("offset = %d, key = %s, value = %s\n",
+ record.offset(), record.key(), record.value());
+ }
+ }
+} 
